f51d268a4338fd08a264e68d3378d664129a4242
rikoudo1er <maxwellwilliam121@gmail.com>
2023-02-13 18:03:48 -0800

    Add file 16

--- 0x02-shell_redirections/File.py
+++ 0x02-shell_redirections/File.py
@@ -2,6 +2,5 @@ import os
 fileName = input("The file name :")
 os.system("echo \"#!/bin/bash \" >"+ fileName)
 os.system("chmod u+x "+ fileName)
-cmd = input("commands : ")
 
-os.system(cmd +">>"+fileName)
+os.system("vim "+fileName)
